0aa8d7aa90b6f480a5da7b03bbd940605a1369c0
oxTunde <phamus1111@gmail.com>
2022-09-06 17:59:32 -0200

    robot cards

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ import './robots'
 
 const Card = ({ id, name, email, }) => {
     return(
-        <div className='flex-wrap tc bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5'>
+        <div className='flex-wrap tc bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5 mt4'>
             <img src= {`https://www.robohash.org/${id}?200x200`} alt='robot' />
             <div>
                 <h1 className=''>{ name }</h1>
--- src/Searcbox.js
+++ src/Searcbox.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import './Searchbox.css'
 
 const Searchbox = () => {
     return(
-        <div className="p3">
+        <div className="p4">
             <input  className="outline p5 b--light-green bg-light-green"
                     type="search" 
                     placeholder="Search For Robots" />
