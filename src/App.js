8edba8fb85d020f2e93c6b344dbc7b44674e9685
oxTunde <phamus1111@gmail.com>
2022-09-08 01:05:22 -0200

    searchbox works

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import Cardlist from './Cardlist'
 import { robots } from './robots'
 import Searchbox from './Searcbox'
 
-
-
 class App extends Component {
-    render (){
-        return(
-            <div className='tc'>
-                <h1 className='green'>Robofriends Site</h1>
-                <Searchbox />
-                <Cardlist className="nb5" robots = { robots } />
-            </div>
-        )
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: '',
     }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const nameFilter = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase())
+    })
+    // const emailFilter = this.state.robots.filter((robots) => {
+    //   return robots.email
+    //     .toLowerCase()
+    //     .includes(this.state.searchfield.toLowerCase())
+    // })
+    return (
+      <div className="tc">
+        <h1 className="green">Robofriends Site</h1>
+        <Searchbox searchChange={this.onSearchChange} />
+        <Cardlist robots={nameFilter} />
+      </div>
+    )
+  }
 }
 export default App
--- src/Searcbox.js
+++ src/Searcbox.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react'
 import './Searchbox.css'
 
-const Searchbox = () => {
-    return(
-        <div className="p4">
-            <input  className="outline p5 b--light-green bg-light-green"
-                    type="search" 
-                    placeholder="Search For Robots" />
-        </div>
-    )
+const Searchbox = ({ searchField, searchChange }) => {
+  return (
+    <div className="p4">
+      <input
+        className="outline p5 b--light-green bg-light-green"
+        type="search"
+        placeholder="Search For Robots"
+        onChange={searchChange}
+      />
+    </div>
+  )
 }
 
-export default Searchbox;
-
+export default Searchbox
